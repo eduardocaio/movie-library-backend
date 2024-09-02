8bb94ee9e7820ed9cd865c3a52b7169b31fcc627
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-02 13:28:19 -0300

    Initialize repository Movie Library Project

---
+++ src/main/java/com/eduardocaio/movie_library_backend/MovieLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eduardocaio.movie_library_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardocaio/movie_library_backend/MovieLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardocaio.movie_library_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
