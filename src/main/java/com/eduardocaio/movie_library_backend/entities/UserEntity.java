93bed7e85fde1503b1ac242784a8c41dd896a348
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-02 14:21:38 -0300

    feat: add UserEntity entity

---
+++ src/main/java/com/eduardocaio/movie_library_backend/entities/UserEntity.java
@@ -0,0 +1,31 @@
+package com.eduardocaio.movie_library_backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+}
