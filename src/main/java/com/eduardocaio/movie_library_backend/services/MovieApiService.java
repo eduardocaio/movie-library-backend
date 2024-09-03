51f090c42c200c11a7ab6bbbf03a7d58e9ef84e4
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-03 11:33:20 -0300

    feat: add MovieApiService for consuming TMDB API in different situations

---
+++ src/main/java/com/eduardocaio/movie_library_backend/services/MovieApiService.java
@@ -0,0 +1,54 @@
+package com.eduardocaio.movie_library_backend.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.eduardocaio.movie_library_backend.dto.MovieDTO;
+import com.eduardocaio.movie_library_backend.dto.MovieResponseDTO;
+
+@Service
+public class MovieApiService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public MovieDTO findMovie(Long id) {
+        String apiKey = "d3c279bcd29bef7b76e244f4459ff5b8";
+        String language = "pt-BR";
+        String url = "https://api.themoviedb.org/3/movie/" + id + "?api_key=" + apiKey + "&language=" + language;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<MovieDTO> response = restTemplate.exchange(url, HttpMethod.GET, entity, MovieDTO.class);
+
+        return response.getBody();
+
+    }
+
+    public List<MovieDTO> discover() {
+        String apiKey = "d3c279bcd29bef7b76e244f4459ff5b8";
+        String language = "pt-BR";
+        String url = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey + "&language=" + language;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<MovieResponseDTO> response = restTemplate.exchange(url, HttpMethod.GET, entity, MovieResponseDTO.class);
+
+        return response.getBody().getResults();
+    }
+
+}
