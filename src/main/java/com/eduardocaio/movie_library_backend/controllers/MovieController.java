513900a774c9c832b56353ab965ac501af98b1f6
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-03 13:15:17 -0300

    feat: add MovieController for API request

---
+++ src/main/java/com/eduardocaio/movie_library_backend/controllers/MovieController.java
@@ -0,0 +1,36 @@
+package com.eduardocaio.movie_library_backend.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eduardocaio.movie_library_backend.dto.MovieDTO;
+import com.eduardocaio.movie_library_backend.services.MovieApiService;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieApiService movieApiService;
+
+    @GetMapping
+    public ResponseEntity<List<MovieDTO>> discover(){
+        return ResponseEntity.ok().body(movieApiService.discoverMovie());
+    }
+
+    public ResponseEntity<List<MovieDTO>> searchMovie(String query){
+        return ResponseEntity.ok().body(movieApiService.searchMovie(query));
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<MovieDTO> findMovieId(@PathVariable("id") Long id){
+        return ResponseEntity.ok().body(movieApiService.findMovie(id));
+    }
+
+}
