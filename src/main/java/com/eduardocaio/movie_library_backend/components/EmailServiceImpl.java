49b3b736c0b621b23e45a49f19aca49eebde4bcf
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-04 12:47:27 -0300

    feat: add email service for new user email confirmation

---
+++ src/main/java/com/eduardocaio/movie_library_backend/components/EmailServiceImpl.java
@@ -0,0 +1,22 @@
+package com.eduardocaio.movie_library_backend.components;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceImpl{
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendSimpleMessage(String to, String subject, String text){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
