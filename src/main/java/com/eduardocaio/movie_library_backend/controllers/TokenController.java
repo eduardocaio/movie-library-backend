82b4bff031459671412261480ee9bf6bb2ebe55d
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-04 13:00:17 -0300

    feat: add endpoint mapping for new user registration and moved login logic to TokenService

--- src/main/java/com/eduardocaio/movie_library_backend/controllers/TokenController.java
+++ src/main/java/com/eduardocaio/movie_library_backend/controllers/TokenController.java
@@ -1,16 +1,13 @@
 package com.eduardocaio.movie_library_backend.controllers;
 
-import java.time.Instant;
+
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.eduardocaio.movie_library_backend.dto.LoginRequest;
 import com.eduardocaio.movie_library_backend.dto.LoginResponse;
-import com.eduardocaio.movie_library_backend.entities.UserEntity;
+import com.eduardocaio.movie_library_backend.dto.UserSignupDTO;
+import com.eduardocaio.movie_library_backend.services.TokenService;
 import com.eduardocaio.movie_library_backend.services.UserService;
 
 @RestController
@@ -27,29 +25,26 @@ import com.eduardocaio.movie_library_backend.services.UserService;
 public class TokenController {
 
     @Autowired
-    private JwtEncoder jwtEncoder;
+    private TokenService tokenService;
 
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
 
     @PostMapping(value = "/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest login){
-        UserEntity user = userService.findByUsername(login.username());
-        if(user == null || !user.isLoginCorrect(login, bCryptPasswordEncoder)){
-            throw new BadCredentialsException("user or password is invalid");
-        }
+        return ResponseEntity.ok(tokenService.login(login));
+    }
 
-        Instant now = Instant.now();
-        Long expiresIn = 300L;
+    @PostMapping(value = "/signup")
+    public ResponseEntity<Void> signup(@RequestBody UserSignupDTO newUser){
+        userService.signup(newUser);
+        return ResponseEntity.ok().build();
+    }
 
-        JwtClaimsSet claims = JwtClaimsSet.builder().issuer("movie-library-eduardocaio").subject(user.getId().toString()).expiresAt(now.plusSeconds(expiresIn)).issuedAt(now).build();
-
-        String jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
-
-        return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
+    @GetMapping(value = "/verify/{code}")
+    public ResponseEntity<Void> verify(@PathVariable("code") UUID code){
+        userService.verify(code);
+        return ResponseEntity.ok().build();
     }
 }
