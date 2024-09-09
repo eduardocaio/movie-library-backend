9f758c5e2048d78c399e8806018967626696feb6
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-09-09 14:03:16 -0300

    refactor: remove AdminUserConfig for tests and add permitAll for OPTIONS request

--- src/main/java/com/eduardocaio/movie_library_backend/configs/SecurityConfig.java
+++ src/main/java/com/eduardocaio/movie_library_backend/configs/SecurityConfig.java
@@ -38,9 +38,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.POST, "/auth/**").permitAll().requestMatchers(HttpMethod.GET, "/auth/**").permitAll().anyRequest().authenticated()).csrf(csrf -> csrf.disable()).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/**").permitAll().requestMatchers(HttpMethod.GET, "/users/{username}/favorite-movies").permitAll().requestMatchers(HttpMethod.GET, "/movies/**").permitAll().requestMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated()).csrf(csrf -> csrf.disable()).oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-
+        
         return http.build();
     }
 
